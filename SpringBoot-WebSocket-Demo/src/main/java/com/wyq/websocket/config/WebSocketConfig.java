4f1403bd7090c75f4c5bbab4691fab9e4f3445dc
Ricardo0324 <2758657741@qq.com>
2022-05-07 00:05:31 +0800

    注释提交

--- SpringBoot-WebSocket-Demo/src/main/java/com/wyq/websocket/config/WebSocketConfig.java
+++ SpringBoot-WebSocket-Demo/src/main/java/com/wyq/websocket/config/WebSocketConfig.java
@@ -2,7 +2,6 @@ package com.wyq.websocket.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
@@ -13,6 +12,9 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
  */
 @Configuration
 public class WebSocketConfig {
+    /**
+     * 这个Bean会自动注册使用@ServerEndpoint注解声明的websocket endpoint
+     */
     @Bean
     public ServerEndpointExporter serverEndpointExporter(){
         return new ServerEndpointExporter();
